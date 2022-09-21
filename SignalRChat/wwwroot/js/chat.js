5d99da98e60f871803c653b5ffcb538c35e033b8
BHARATJOHRI <BHARATJOHRI18@gmail.com>
2022-09-21 13:42:46 +0530

    first

---
+++ SignalRChat/wwwroot/js/chat.js
@@ -0,0 +1,21 @@
+﻿"use strict";
+var connection = new signalR.HubConnectionBuilder().withUrl("/chatHub").build();
+
+document.getElementById("sendButton").disabled = true;
+
+connection.on("ReceiveMessage", function (user, message) {
+    var li = document.createElement("li");
+    document.getElementById("messagesList").appendChild(li);
+    li.textContent = `${user} says ${message}`;
+});
+
+connection.start().then(function () {
+    document.getElementById("sendButton").disabled = false;
+});
+
+document.getElementById("sendButton").addEventListener("click", function (event) {
+    var user = document.getElementById("userInput").value;
+    var message = document.getElementById("messageInput").value;
+    connection.invoke("SendMessage", user, message);
+    event.preventDefault();
+});
